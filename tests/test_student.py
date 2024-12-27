c1b8a555a6fbdf3e3d2a6158516583bb7baedae4
Sanjana Mathi <sanjuvimal@icloud.com>
2024-12-27 20:47:32 +0100

    added test features

--- tests/test_student.py
+++ tests/test_student.py
@@ -5,13 +5,12 @@ import sqlite3
 from unittest.mock import patch
 
 
-class TestConfig(unittest.TestCase):
+class TestStudent(unittest.TestCase):
     def setUp(self):
         self.file_name = "students_test.db"
         self.student_manager = StudentManager(self.file_name)
 
     def test_create_table(self):
-        self.assertTrue(self.student_manager.create_table())
         pass
 
     def test_write_command():
@@ -26,14 +25,21 @@ class TestConfig(unittest.TestCase):
     def test_create_student_from_sql():
         pass
 
-    def test_add_student():
-        pass
+    def test_add_student(self):
+        self.student_manager.add_student("Sanjana", 23, "VG", "maths")
+        students = self.student_manager.view_all_students()
+        self.assertIn("Sanjana", [student["name"] for student in students.items()])
 
-    def test_view_all_students():
-        pass
+    def test_view_all_students(self):
+        self.student_manager.add_student("Vimal", 23, "VG", "maths")
+        students = self.student_manager.view_all_students()
+        self.assertIn("Vimal", [student["name"] for student in students.items()])
 
-    def test_view_specifik_student():
-        pass
+    def test_view_specifik_student(self):
+        self.student_manager.add_student("Shan", 23, "VG", "maths")
+        student_id = self.student_manager.get_id_list()
+        student = self.student_manager.view_specifik_student(student_id)
+        self.assertEqual(student["Name"], "Shan")
 
     def test_update_student():
         pass
